66733c444c569236c2de98faba1e4ec3d9b1d2c7
Nic_ <36131101+NicCraver@users.noreply.github.com>
2020-01-15 17:51:07 +0800

    编写"通用广告"组件

---
+++ nic-react-blog/components/Advert.js
@@ -0,0 +1,26 @@
+import { Avatar, Divider } from 'antd'
+import '../public/style/components/advert.css'
+
+const Author = () => {
+  return (
+    <div className="advert-div comm-box">
+      <div>
+        <img src="http://newimg.jspang.com/zhishixingqiu.png" width="100%" />
+      </div>
+      <div>
+        <img src="http://newimg.jspang.com/zhishixingqiu.png" width="100%" />
+      </div>
+      <div>
+        <img src="http://newimg.jspang.com/zhishixingqiu.png" width="100%" />
+      </div>
+      <div>
+        <img src="http://newimg.jspang.com/zhishixingqiu.png" width="100%" />
+      </div>
+      <div>
+        <img src="http://newimg.jspang.com/zhishixingqiu.png" width="100%" />
+      </div>
+    </div>
+  )
+}
+
+export default Author
--- nic-react-blog/pages/index.js
+++ nic-react-blog/pages/index.js
@@ -4,6 +4,7 @@ import { Row, Col, List, Icon } from 'antd'
 import '../public/style/pages/index.css'
 import Header from '../components/Header'
 import Author from '../components/Author'
+import Advert from '../components/Advert'
 
 const Home = () => {
   const [mylist, setMylist] = useState([
@@ -66,6 +67,7 @@ const Home = () => {
 
         <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
           <Author />
+          <Advert />
         </Col>
       </Row>
     </>
